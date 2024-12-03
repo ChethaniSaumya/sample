942aead56e26517496e7ee8edfff07fa4b942dce
Dominic Brian <dominic91960@gmail.com>
2024-12-04 03:51:22 +0530

    feat(fonts): add fonts vermin-vibes-v, montserrat-alternates, saira-condensed and reem-kufi-fun

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      "vermin-vibes-v": [
+        "Vermin Vibes V",
+        "Impact",
+        "Arial Black",
+        "sans-serif",
+      ],
+      "montserrat-alternates": [
+        "Montserrat Alternates",
+        "Arial",
+        "Helvetica",
+        "sans-serif",
+      ],
+      "saira-condensed": [
+        "Saira Condensed",
+        "Roboto Condensed",
+        "Arial Narrow",
+        "sans-serif",
+      ],
+      "reem-kufi-fun": [
+        "Reem Kufi Fun",
+        "Comic Sans MS",
+        "Poppins",
+        "sans-serif",
+      ],
+    },
     extend: {},
   },
   plugins: [],
