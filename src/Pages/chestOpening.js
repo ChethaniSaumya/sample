f6264c4bed73fce82b40571bd29dd927e87d859e
Chethani Saumya <105449384+ChethaniSaumya@users.noreply.github.com>
2024-12-25 02:40:46 +0530

    Update chestOpening.js

--- src/Pages/chestOpening.js
+++ src/Pages/chestOpening.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import "../styles/ChestOpening.css";
+import '../styles/carousel.css';
 
 import cl from '../assets/chainlink.png';
 import chest from '../assets/chest.png';
@@ -10,59 +11,75 @@ import treasure from "../assets/Comp 1_00088.png";
 import cz from "../assets/cz-without-bg.png";
 import "../styles/Home.css";
 import cardLayout from "../assets/card-layout.png";
-import vitalip from "../assets/heroes2/vitalip.png";
-import richard from "../assets/heroes2/richard.png";
-import gz from "../assets/heroes2/gz.png";
-import trunk from "../assets/heroes2/trunk.png";
-import jesus from "../assets/heroes2/jesus.png";
-import elom from "../assets/heroes2/elon.png";
-import satoshi from "../assets/heroes2/satoshi.png";
 
-import vitalip2 from "../assets/heroes/vitalip.png";
-import richard2 from "../assets/heroes/richard.png";
-import gz2 from "../assets/heroes/gz.png";
-import trunk2 from "../assets/heroes/trunk.png";
-import jesus2 from "../assets/heroes/jesus.png";
-import elom2 from "../assets/heroes/elom.png";
-import satoshi2 from "../assets/heroes/satoshi.png";
+import vitalip from "../assets/heroes/vitalip.png";
+import richard from "../assets/heroes/richard.png";
+import gz from "../assets/heroes/gz.png";
+import trunk from "../assets/heroes/trunk.png";
+import jesus from "../assets/heroes/jesus.png";
+import elom from "../assets/heroes/elom.png";
+import satoshi from "../assets/heroes/satoshi.png";
+
+import vitalip2 from "../assets/heroes2/vitalip.png";
+import richard2 from "../assets/heroes2/richard.png";
+import gz2 from "../assets/heroes2/gz.png";
+import trunk2 from "../assets/heroes2/trunk.png";
+import jesus2 from "../assets/heroes2/jesus.png";
+import elom2 from "../assets/heroes2/elon.png";
+import satoshi2 from "../assets/heroes2/satoshi.png";
+
+
+import $ from 'jquery';
+import Carousel from '../components/carousel';
+import right from "../assets/heroes2/right.png";
 
 var Scroll = require('react-scroll');
 
 var Link = Scroll.Link;
 var Element = Scroll.Element;
 
+
+
+
 const heroData = [
 	{
-		image: vitalip2,
+		image: vitalip,
 		name: "vitalip",
 	},
 	{
-		image: richard2,
+		image: richard,
 		name: "richard",
 	},
 	{
-		image: gz2,
+		image: gz,
 		name: "gz",
 	},
 	{
-		image: trunk2,
+		image: trunk,
 		name: "trunk",
 	},
 	{
-		image: jesus2,
+		image: jesus,
 		name: "jesus",
 	},
 	{
-		image: elom2,
+		image: elom,
 		name: "elom",
 	},
 	{
-		image: satoshi2,
+		image: satoshi,
 		name: "satoshi",
 	},
 ];
 
+
 const ChestOpening = () => {
+
+	const carouselTrackRef = useRef(null); // Ref for the carousel track
+	let lapTime = 1.8; // Initial animation duration in seconds
+	let animationActive = false; // To prevent multiple animations running simultaneously
+
+
 	const [_navbarOpen, setNavbarOpen] = useState(0);
 	const [isDropdownVisible, setDropdownVisible] = useState(false);
 	const [loading, setLoading] = useState(true);
@@ -72,16 +89,24 @@ const ChestOpening = () => {
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const [carouselRun, setCarouselRun] = useState(0);
 	const [carouselRunMob, setCarouselRunMob] = useState(0);
+	const [arrowShow, setArrowShow] = useState(0);
 
 
 	const [cardShow, setCardShow] = useState(0);
 	const [spreadRun, setSpreadRun] = useState(0);
+	const [isSlowing, setIsSlowing] = useState(false);
+	const [_lapTime, setLapTime] = useState(0.6);
 
-	const [showSecondDiv, setShowSecondDiv] = useState(0);
-	const nftSectionRef = useRef(null); // Create a reference
+	const [number, setNumber] = useState(null);
+
+	const generateRandomNumber = () => {
+		const randomNum = Math.floor(Math.random() * 7) + 1; // Generate a random number between 1 and 7
+		setNumber(randomNum);
+	};
 
 
 	const chestOpen = async () => {
+		generateRandomNumber();
 		set_chestOpen(1);
 
 		await new Promise(resolve => setTimeout(resolve, 4500));
@@ -94,6 +119,7 @@ const ChestOpening = () => {
 	};
 
 	const chestOpenMob = async () => {
+		generateRandomNumber();
 		set_chestOpen(1);
 
 		await new Promise(resolve => setTimeout(resolve, 4500));
@@ -105,8 +131,6 @@ const ChestOpening = () => {
 */
 	};
 
-
-
 	const carouseOpen = async () => {
 		// Scroll to the nftSection
 		setSpreadRun(1);
@@ -114,12 +138,14 @@ const ChestOpening = () => {
 
 		await new Promise(resolve => setTimeout(resolve, 2300));
 		setCarouselRun(1);
+		setArrowShow(1);
 		setSpreadRun(0);
 
-		await new Promise(resolve => setTimeout(resolve, 10000));
+		await new Promise(resolve => setTimeout(resolve, 24000));
 
 		setCardShow(1);
-		await new Promise(resolve => setTimeout(resolve, 6000));
+		setArrowShow(0);
+		await new Promise(resolve => setTimeout(resolve, 5000));
 		setCarouselRun(0);
 		await new Promise(resolve => setTimeout(resolve, 5000));
 		setCardShow(0);
@@ -128,15 +154,18 @@ const ChestOpening = () => {
 		window.location.reload(true);
 	};
 
+
 	const carouseOpenMob = async () => {
 		// Scroll to the nftSection
 		setCarouselRunMob(1);
+		setArrowShow(1);
 		set_chestOpen(0);
 
-		await new Promise(resolve => setTimeout(resolve, 10000));
+		await new Promise(resolve => setTimeout(resolve, 24000));
 
 		setCardShow(1);
-		await new Promise(resolve => setTimeout(resolve, 6000));
+		setArrowShow(0);
+		await new Promise(resolve => setTimeout(resolve, 5000));
 		setCarouselRun(0);
 		await new Promise(resolve => setTimeout(resolve, 5000));
 		setCardShow(0);
@@ -152,6 +181,37 @@ const ChestOpening = () => {
 	const toggleDropdown = () => {
 		setIsOpen(!isOpen);
 	};
+
+	/*useEffect(() => {
+
+		$(document).ready(function () {
+			const carouselTrack = $(".carousel-track");
+			let lapTime = 0.6; // Initial animation duration in seconds
+
+			console.log("2222222222");
+
+
+			// Function to start animation with the current lap time
+			function startCarousel() {
+				carouselTrack.css({
+					"animation-duration": `${lapTime}s`,
+					animation: `carousel ${lapTime}s linear infinite`,
+				});
+
+				// Listen for the animationend event to update the lap time
+				carouselTrack.one("animationiteration", function () {
+					lapTime += 0.2; // Increase lap time by 0.2 seconds
+					setLapTime(lapTime);
+					startCarousel(); // Restart animation with updated duration
+				});
+			}
+
+			// Start the carousel animation
+			//startCarousel();
+		});
+
+
+	}, [_lapTime]);*/
 
 
 	useEffect(() => {
@@ -181,95 +241,50 @@ const ChestOpening = () => {
 			<Navbar />
 
 			<div className='cont-2'>
-				<div className='titleMain'>FIND YOUR PULSE HERO <span>NFT</span></div>
+				<div className='titleMain'>FIND YOUR PULSE HEROS <span>NFT</span></div>
 				<div className='title2Main'>Secured by <img className='chainlink' src={cl} /></div>
 
 				<div className='nftSection'>
- 					{_chestOpen > 0 ?
+					{_chestOpen > 0 ?
 						<div className='treasureIMG2Div'>
 							<video id="treasureIMG2" className={`${cardShow > 0 ? "fade-out" : ""
 								} ${carouselRun > 0 ? "hidden" : ""}`} autoPlay loop muted playsInline>
 								<source src={chestAnim} type="video/webm" />
 							</video></div> :
 						<div className="video-container"><img className='treasureIMG' src={treasure} /></div>}
- 
+
 					<div>
+
 						{spreadRun > 0 ?
 
-							<div className="carouselImgs">
-								<img src={vitalip} style={{ "--target-left": "10vw" }} alt="Vitalip" />
-								<img src={richard} style={{ "--target-left": "23vw" }} alt="Richard" />
-								<img src={gz} style={{ "--target-left": "36vw" }} alt="GZ" />
-								<img src={trunk} style={{ "--target-left": "49vw" }} alt="Trunk" />
-								<img src={jesus} style={{ "--target-left": "62vw" }} alt="Jesus" />
-								<img src={elom} style={{ "--target-left": "75vw" }} alt="Elom" />
-								<img src={satoshi} style={{ "--target-left": "88vw" }} alt="Satoshi" />
+							<div className="carouselImgs-2">
+								<img src={vitalip2} style={{ "--target-left": "10vw" }} alt="Vitalip" />
+								<img src={richard2} style={{ "--target-left": "23vw" }} alt="Richard" />
+								<img src={gz2} style={{ "--target-left": "36vw" }} alt="GZ" />
+								<img src={trunk2} style={{ "--target-left": "49vw" }} alt="Trunk" />
+								<img src={jesus2} style={{ "--target-left": "62vw" }} alt="Jesus" />
+								<img src={elom2} style={{ "--target-left": "75vw" }} alt="Elom" />
+								<img src={satoshi2} style={{ "--target-left": "88vw" }} alt="Satoshi" />
 
 							</div> : null}
 
-						{carouselRun > 0 ?
-							<div
-								id="rowImgs"
-								className={`relative overflow-hidden container mx-auto pb-[100px] z-[1] ${cardShow > 0 ? "fade-out" : ""
-									} ${cardShow > 0 && carouselRun === 0 ? "hidden" : ""}`}
-							>
-								<div className="carousel-track flex w-fit animate-carousel">
-									{/* Duplicate images for seamless looping */}
-									{heroData.concat(heroData).map(({ image, name }, index) => (
-										<article
-											key={index}
-											className="relative w-fit max-w-fit mx-3 my-10 flex items-end justify-end transition-transform duration-300 hover:-translate-y-1"
-										>
-											<img
-												src={cardLayout}
-												alt="Card layout"
-												style={{
-													width: '10vw', // Adjust width based on viewport width
-													height: '14vw', // Adjust height based on viewport width
-												}}
-											/>
-											<div
-												className="absolute inset-0 w-[90%] h-[98%] m-auto bg-contain bg-no-repeat bg-center"
-												style={{ backgroundImage: `url(${image})` }}
-											></div>
-											<p id="sizeFonts">
-												{name}
-											</p>
-										</article>
-									))}
-								</div>
-							</div>
-							: null}
- 
+						<div>
+							{carouselRun > 0 ? (
+								<>
+									<Carousel number={number} />
+									{arrowShow > 0 ?
+										<div class="highlighter2"><img className='arrow' src={right} /></div> : null}
+								</>
+							) : null}
+
+						</div>
+
 						{carouselRunMob > 0 ?
-							<div
-								id="rowImgs"
-								className={`relative overflow-hidden container mx-auto pb-[100px] z-[1] ${cardShow > 0 ? "fade-out" : ""
-									} ${cardShow > 0 && carouselRun === 0 ? "hidden" : ""}`}
-							>
-								<div className="carousel-track flex w-fit animate-carousel">
-									{/* Duplicate images for seamless looping */}
-									{heroData.concat(heroData).map(({ image, name }, index) => (
-										<article
-											key={index}
-											className="relative w-fit max-w-fit mx-5 my-10 flex items-end justify-end transition-transform duration-300 hover:-translate-y-1"
-										>
-											<img
-												src={cardLayout}
-												alt="Card layout"
-												className="w-[122px] h-[170px] sm:w-[136px] sm:h-[188px] md:w-[150px] md:h-[206px] lg:w-[162px] lg:h-[220px] xl:w-[170px] xl:h-[226px] 2xl:w-[176px] 2xl:h-[232px]"
-											/>
-											<div
-												className="absolute inset-0 w-[90%] h-[98%] m-auto bg-contain bg-no-repeat bg-center"
-												style={{ backgroundImage: `url(${image})` }}
-											></div>
-											<p className="absolute bottom-0 left-0 right-0 w-fit mx-auto translate-y-full font-vermin-vibes-v text-[19px] z-[1]">
-												{name}
-											</p>
-										</article>
-									))}
-								</div>
-							</div>
+							<>
+								<Carousel number={number} />
+								{arrowShow > 0 ?
+									<div class="highlighter2"><img className='arrow' src={right} /></div> : null}
+							</>
 							: null}
 
 					</div>
@@ -278,31 +293,63 @@ const ChestOpening = () => {
 
 						{cardShow > 0 ?
 							<div className='popUpImgMain'>
-								<div className='popUpImg'>
-									<span class="close-button" onClick={close}>&times;</span>
 
-									<article
-										className="relative w-fit max-w-fit mx-5 my-10 flex items-end justify-end transition-transform duration-300 hover:-translate-y-1 magic-card"
-									>
-										<img
-											src={cardLayout}
-											alt="Card layout"
-											className="w-[122px] h-[170px] sm:w-[136px] sm:h-[188px] md:w-[150px] md:h-[206px] lg:w-[162px] lg:h-[220px] xl:w-[170px] xl:h-[226px] 2xl:w-[176px] 2xl:h-[232px]"
-										/>
-										<div
-											className="absolute inset-0 w-[90%] h-[98%] m-auto bg-contain bg-no-repeat bg-center"
-											style={{ backgroundImage: `url(${cz})` }}
-										></div>
-										<p className="absolute bottom-0 left-0 right-0 w-fit mx-auto translate-y-full font-vermin-vibes-v text-[19px] z-[1]">
-											CZ
-										</p>
+								{number === 1 &&
+									<>
+										<span class="close-button-2" onClick={close}>&times;</span>
+
+										<img src={trunk2} />
+									</>
+								}
+
+								{number === 2 &&
+									<>
+										<span class="close-button-2" onClick={close}>&times;</span>
+
+										<img src={vitalip2} />
+									</>
+								}
+
+								{number === 3 &&
+									<>
+										<span class="close-button-2" onClick={close}>&times;</span>
+
+										<img src={richard2} />
+									</>}
+
+								{number === 4 &&
+									<>
+										<span class="close-button-2" onClick={close}>&times;</span>
+
+										<img src={gz2} />
+									</>}
+
+								{number === 5 &&
+									<>
+										<span class="close-button-2" onClick={close}>&times;</span>
+
+										<img src={jesus2} />
+									</>}
+
+								{number === 6 &&
+									<>
+										<span class="close-button-2" onClick={close}>&times;</span>
+
+										<img src={elom2} />
+									</>}
+
+								{number === 7 &&
+									<>
+										<span class="close-button-2" onClick={close}>&times;</span>
+
+										<img src={satoshi2} />
+									</>}
 
 
-									</article>
-								</div>
-							</div> : null}
-
+							</div>
+							: null}
 					</div>
+
 				</div>
 
 				<div className='btns3'>
