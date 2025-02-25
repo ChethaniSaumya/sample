793d864834708098432227d0fbbd3fb84542a219
Chethani Saumya <105449384+ChethaniSaumya@users.noreply.github.com>
2025-02-26 02:07:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home";
-import Fight from "./Pages/Fight";
+import Vs from "./Pages/Fight";
 import ChestOpening from "./Pages/chestOpening";
 import SecBattle from "./Pages/SecBattle";
 // import Marketplace from "./Pages/Marketplace";
 import MarketplaceV2 from "./Pages/MarketplaceV2";
 import Inventory from './Pages/Inventory';
+import Vs2 from "./Pages/Fight2";
 
 function App() {
   return (
@@ -14,13 +15,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="battle" element={<Fight />} />
+          <Route path="battle" element={<Vs />} />
           <Route path="sec-batttle" element={<SecBattle />} />
-          <Route path="buy-chests" element={<ChestOpening />} />
-          {/* <Route path='marketplace' element={<Marketplace />} /> */}
+          {/*<Route path="chest-presale" element={<ChestOpening />} />*/}
           <Route path="marketplace" element={<MarketplaceV2 />} />
  					<Route path='mynft' element={<Inventory />} />
-
+ 					<Route path="/battle2/:token_id/:rarity" element={<Vs2 />} />
 
         </Routes>
       </BrowserRouter>
